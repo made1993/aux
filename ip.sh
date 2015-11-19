4b60514b7eb36bed49195d1f78ec43ad28d543d0
made1993 <mariogr_93@hotmail.com>
2015-11-19 22:11:05 +0000

    first commit

--- ip.sh
+++ ip.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 while true
 do
-	sleep 3
-	echo "hola"
 	curl ifconfig.me -o ip.txt
-	echo "hola" >>ip.txt
 
 	
 	git add ip.sh
 	git commit -m "first commit"
 	git push -u origin master
+	sleep 1500
 done
